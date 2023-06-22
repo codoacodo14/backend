32d0ab4f0746e96f7665c03b51cff7e1e57ad6a9
Viviana-adia <83620424+Viviana-adia@users.noreply.github.com>
2023-06-22 16:35:44 -0300

    cambios tablas

--- library_catalog_.py
+++ library_catalog_.py
@@ -4,15 +4,16 @@ import bs4 as bs
 import requests
 import pandas as pd
 
-#Llamada a la página web. Se traen las tablas, cada una contine datos de 1 libro
-#Dos columnas, la de la izquierda con el nombre de los datos (categorías), la derecha con los datos
-#Cada fila es una lista con dos elementos ([0] categorías; [1] datos). 28 filas por tabla
+# Llamada a la página web. Se traen las tablas, cada una contine datos de 1 libro
+# Dos columnas, la de la izquierda con el nombre de los datos (categorías), la derecha con los datos
+# Cada fila es una lista con dos elementos ([0] categorías; [1] datos). 28 filas por tabla
 
 data = []
 
 
-for i in range(1,400):
-    url = 'http://catalogo.bibliotecas.gob.ar/pergamo/opac/cgi-bin/pgopac.cgi?VDOC=1.'+str(i) + '#Ejemplares'
+for i in range(1, 400):
+    url = 'http://catalogo.bibliotecas.gob.ar/pergamo/opac/cgi-bin/pgopac.cgi?VDOC=1.' + \
+        str(i) + '#Ejemplares'
     resp = requests.get(url)
 
     resp = resp.text
@@ -26,8 +27,8 @@ for i in range(1,400):
 
 len(data)
 
-#Creamos un diccionario para agrupar los datos según el nombre de cada uno. Y luego pasamos los datos agrupados a listas
-#Resulta una lista (result) de 28 listas (cada una contiene los datos de las categorías de las tablas)
+# Creamos un diccionario para agrupar los datos según el nombre de cada uno. Y luego pasamos los datos agrupados a listas
+# Resulta una lista (result) de 28 listas (cada una contiene los datos de las categorías de las tablas)
 d = {}
 for subl in data:
     try:
@@ -45,20 +46,23 @@ len(data)
 
 len(result)
 
-#De las 399 iteraciones, quedan 299 puesto que en medio había listas vacías
+# De las 399 iteraciones, quedan 299 puesto que en medio había listas vacías
 len(result[4])
 
-#Se crea una función para pasar los elementos de las listas de 'td' HTML a text
+# Se crea una función para pasar los elementos de las listas de 'td' HTML a text
+
+
 def pass_to_text(list_):
     text_list = []
     for tag in list_:
         text_list.append(tag.get_text())
 
-    return(text_list)
+    return (text_list)
 
-#Se llama a la función para iterar sobre las listas con los datos que nos interesan
+
+# Se llama a la función para iterar sobre las listas con los datos que nos interesan
 isbn = pass_to_text(result[0])
-tipo_texto=pass_to_text(result[1])
+tipo_texto = pass_to_text(result[1])
 titulo = pass_to_text(result[4])
 autor = pass_to_text(result[7])
 editorial = pass_to_text(result[10])
@@ -82,15 +86,15 @@ for elem in materia_0:
     else:
         materia.append(elem)
 
-#Se crea un DataFrame con pandas
-df = pd.DataFrame(list(zip(isbn,tipo_texto, titulo, autor, editorial, fecha_edicion, lugar_edicion, materia,fecha_alta)),
-                  columns=['ISBN','Tipo_Texto', 'Título', 'Autor','Editorial','Fecha_Edición','Lugar_Edición','Materia','Fecha_Alta'])
+# Se crea un DataFrame con pandas
+df = pd.DataFrame(list(zip(isbn, tipo_texto, titulo, autor, editorial, fecha_edicion, lugar_edicion, materia, fecha_alta)),
+                  columns=['ISBN', 'Tipo_Texto', 'Título', 'Autor', 'Editorial', 'Fecha_Edición', 'Lugar_Edición', 'Materia', 'Fecha_Alta'])
 df = df.reset_index(drop=True)
 df
 
 len(df)
 
-#Se exportan los datos a archivos csv y xlsx
+# Se exportan los datos a archivos csv y xlsx
 
 df.to_csv('library_catalog.csv', index=False)
 
@@ -100,7 +104,8 @@ df.to_excel("library_catalog.xlsx", index=False)
 autores = {autor: i for i, autor in enumerate(df['Autor'].unique(), start=1)}
 editoriales = {editorial: i for i,
                editorial in enumerate(df['Editorial'].unique(), start=1)}
-materias = {materia: i for i, materia in enumerate(df['Materia'].unique(), start=1)}
+materias = {materia: i for i, materia in enumerate(
+    df['Materia'].unique(), start=1)}
 
 
 # Copiamos los datos de la tabla original y mapeamos los datos de las columnas que nos interesan por su id
@@ -109,6 +114,11 @@ df2['Autor'] = df2['Autor'].map(autores)
 df2['Editorial'] = df2['Editorial'].map(editoriales)
 df2['Materia'] = df2['Materia'].map(materias)
 df2
+
+# Creamos nueva tabla de libros con ids
+df2.to_csv('books.csv', index=False)
+
+df2.to_excel("books.xlsx", index=False)
 
 
 # Creamos nuevas tablas con id
